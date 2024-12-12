ebb45c6007e33054d3ef838b08b517c773d768db
Tarnbir Singh <tarnbir.singh@sap.com>
2024-12-12 19:45:35 +0100

    Branch created

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,14 @@
 import * as usersSchema from "./models/users.ts";
 import * as moviesSchema from "./models/movies.ts";
 
-export const schema = {
-  ...usersSchema,
-  ...moviesSchema,
-};
+export const users = pgTable('users', {
+	id: varchar('clerk_id').notNull().unique().primaryKey(),
+	email: varchar('email').notNull().unique(),
+	firstName: varchar('first_name'),
+	lastName: varchar('last_name'),
+	userName: varchar('user_name'),
+	imageUrl: varchar('image_url'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at').defaultNow(),
+  });
+  
