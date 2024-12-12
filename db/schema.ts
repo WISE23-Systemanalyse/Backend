9a29c08e169d5aee6a5a281df36d8b19ed8b284d
Dilmand Saado <dilmand@outlook.de>
2024-12-12 20:16:24 +0100

    Refactor: update schema definitions for user tables

--- db/schema.ts
+++ db/schema.ts
@@ -1,14 +1,21 @@
-import * as usersSchema from "./models/users.ts";
-import * as moviesSchema from "./models/movies.ts";
+import { pgTable, text, integer, varchar, timestamp, uniqueIndex } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
 
-export const users = pgTable('users', {
-	id: varchar('clerk_id').notNull().unique().primaryKey(),
-	email: varchar('email').notNull().unique(),
-	firstName: varchar('first_name'),
-	lastName: varchar('last_name'),
-	userName: varchar('user_name'),
-	imageUrl: varchar('image_url'),
-	createdAt: timestamp('created_at').defaultNow(),
-	updatedAt: timestamp('updated_at').defaultNow(),
-  });
-  
+
+
+export const movie = pgTable("movie", {
+	id: integer().primaryKey().notNull(),
+	title: text().notNull(),
+	year: integer().notNull(),
+});
+
+export const user = pgTable("users", {
+  id: varchar("clerk_id").notNull().unique().primaryKey(),
+  email: varchar("email").notNull().unique(),
+  firstName: varchar("first_name"),
+  lastName: varchar("last_name"),
+  userName: varchar("user_name"),
+  imageUrl: varchar("image_url"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
