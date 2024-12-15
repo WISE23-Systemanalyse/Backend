e5dee67d13d8924e96260f1bcccb19ca54c5a3b6
Dilmand Saado <dilmand@outlook.de>
2024-12-15 18:05:10 +0100

    feat: implement repositories for halls, seats, and shows with CRUD operations

---
+++ db/repositories/seats.ts
@@ -0,0 +1,28 @@
+import { db } from "../db.ts";
+import { Create, Repository } from "../../interfaces/repository.ts";
+import { seats, Seat } from "../models/seats.ts";
+import { eq } from "drizzle-orm";
+
+export class SeatRepository implements Repository<Seat> {
+    async findAll(): Promise<Seat[]> {
+        const allSeats = await db.select().from(seats);
+        return allSeats;
+    }
+    async find(id: Seat['id']): Promise< Seat | null> {
+        const result = await db.query.seats.findFirst({
+            where: eq(seats.id, id),
+          });
+          return result ?? null;
+    }
+    async delete(id: Seat['id']): Promise<void> {
+        await db.delete(seats).where(eq(seats.id, id));
+    }
+    async create(value: Create<Seat>): Promise<Seat> {
+      const [seat] = await db.insert(seats).values(value).returning();
+      return seat;
+    }
+    async update(id: Seat['id'], value: Create<Seat>): Promise<Seat> {
+        const [updatedSeat] = await db.update(seats).set(value).where(eq(seats.id, id)).returning();
+        return updatedSeat;
+    }
+}
