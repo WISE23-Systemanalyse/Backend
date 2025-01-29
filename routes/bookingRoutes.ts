bbd17b65321c1541f5bca67dbb063b81d44bd43f
Leon Priemer <leon.priemer@gmail.com>
2025-01-29 17:34:42 +0100

    Added new route: bookings/details

--- routes/bookingRoutes.ts
+++ routes/bookingRoutes.ts
@@ -5,8 +5,9 @@ import { bookingController } from "../controllers/index.ts";
 const bookingRoutes = new Router();
 
 bookingRoutes
-    .get("/bookings/:id", bookingController.getOne)
+    .get("/bookings/details", bookingController.getAllBookingDetails)
     .get("/bookings", bookingController.getAll)
+    .get("/bookings/:id", bookingController.getOne)
     .post("/bookings", bookingController.create)
     .put("/bookings/:id", bookingController.update)
     .delete("/bookings:id", bookingController.delete);
